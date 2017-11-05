37b07c16065fb8613f7736e601d1bc763dfc4d2a
Maxwell Haley <maxwell.haley@openmailbox.org>
2017-11-05 15:19:06 -0500

    Allow sharing of options
    Options are now shared inside a SharePreferences object. This means the
    users options are carried around the entire app.

--- app/src/main/java/ca/qc/cegep_heritage/mhaley_b51_mathexplosion/MainActivity.java
+++ app/src/main/java/ca/qc/cegep_heritage/mhaley_b51_mathexplosion/MainActivity.java
@@ -1,13 +1,20 @@
 package ca.qc.cegep_heritage.mhaley_b51_mathexplosion;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.content.Context;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +33,16 @@ public class MainActivity extends AppCompatActivity {
                 switchOptionsActivity();
             }
         });
+
+        SharedPreferences sharedPrefs = MainActivity.this.getSharedPreferences("options", Context.MODE_PRIVATE);
+
+        if(sharedPrefs.getInt("difficulty", -1) == -1) {
+            sharedPrefs.edit().putString("operators", "+-").apply();
+            sharedPrefs.edit().putInt("difficulty", 2).apply();
+            sharedPrefs.edit().putInt("operations", 0).apply();
+
+        }
+
     }
 
     private void switchOptionsActivity() {
--- app/src/main/java/ca/qc/cegep_heritage/mhaley_b51_mathexplosion/OptionsActivity.java
+++ app/src/main/java/ca/qc/cegep_heritage/mhaley_b51_mathexplosion/OptionsActivity.java
@@ -1,12 +1,18 @@
 package ca.qc.cegep_heritage.mhaley_b51_mathexplosion;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Spinner;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
 
 public class OptionsActivity extends AppCompatActivity {
 
@@ -15,36 +21,132 @@ public class OptionsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
 
-        Spinner spnnrDifficulty = (Spinner) findViewById(R.id.spnnrDifficulty);
-        CheckBox chckbxAddition = (CheckBox) findViewById(R.id.chckbxAddition);
-        CheckBox chckbxSubraction = (CheckBox) findViewById(R.id.chckbxSubtraction);
-        CheckBox chckbxMultiplication = (CheckBox) findViewById(R.id.chckbxMultiplication);
-        CheckBox chckbxDivision = (CheckBox) findViewById(R.id.chckbxDivision);
+        final CheckBox chckbxAddition = (CheckBox) findViewById(R.id.chckbxAddition);
+        final CheckBox chckbxSubraction = (CheckBox) findViewById(R.id.chckbxSubtraction);
+        final CheckBox chckbxMultiplication = (CheckBox) findViewById(R.id.chckbxMultiplication);
+        final CheckBox chckbxDivision = (CheckBox) findViewById(R.id.chckbxDivision);
+
         final CheckBox[] operatorCheckboxes = {chckbxAddition, chckbxSubraction, chckbxMultiplication,
                 chckbxDivision };
-        CheckBox chckbxAllOperations = (CheckBox) findViewById(R.id.chckbxAllOperators);
+
+        final CheckBox chckbxAllOperations = (CheckBox) findViewById(R.id.chckbxAllOperators);
+        chckbxAllOperations.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                setCheckboxes(!isChecked, operatorCheckboxes);
+            }
+        });
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.difficulty_options, R.layout.support_simple_spinner_dropdown_item);
 
+        final Spinner spnnrDifficulty = (Spinner) findViewById(R.id.spnnrDifficulty);
         spnnrDifficulty.setAdapter(adapter);
 
-        chckbxAllOperations.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    for (CheckBox operatorCheckbox : operatorCheckboxes) {
-                        operatorCheckbox.setChecked(false);
-                        operatorCheckbox.setClickable(false);
-                        operatorCheckbox.setTextColor(Color.parseColor("#CCCCCC"));
-                    }
-                } else {
-                    for (CheckBox operatorCheckbox : operatorCheckboxes) {
-                        operatorCheckbox.setClickable(true);
-                        operatorCheckbox.setTextColor(Color.parseColor("#000000"));
-                    }
+        // Load Shared Preferences
+        final SharedPreferences sharedPrefs = OptionsActivity.this.getSharedPreferences("options", Context.MODE_PRIVATE);
+
+        if(sharedPrefs.getString("operators", "+").equals("all")) {
+            setCheckboxes(false, operatorCheckboxes);
+            chckbxAllOperations.setChecked(true);
+        } else {
+            String[] selectedOperators = sharedPrefs.getString("operators", "+").split("");
+            for (String selectedOperator : selectedOperators) {
+                switch (selectedOperator) {
+                    case "+":
+                        chckbxAddition.setChecked(true);
+                        break;
+                    case "-":
+                        chckbxSubraction.setChecked(true);
+                        break;
+                    case "*":
+                        chckbxMultiplication.setChecked(true);
+                        break;
+                    case "/":
+                        chckbxDivision.setChecked(true);
+                        break;
+                    default:
                 }
+            }
+        }
+
+        spnnrDifficulty.setSelection(sharedPrefs.getInt("difficulty", 0));
+
+        final RadioButton rdoSingleOperation = (RadioButton) findViewById(R.id.rdoSingleOperation);
+        RadioButton rdoDoubleOperation = (RadioButton) findViewById(R.id.rdoDoubleOperation);
+
+        if (sharedPrefs.getInt("operations", 0) == 0) {
+            rdoSingleOperation.setChecked(true);
+        } else {
+            rdoDoubleOperation.setChecked(true);
+        }
+
+        // Saving user preferences
+        Button btnSaveOptions = (Button) findViewById(R.id.btnSaveOptions);
+        btnSaveOptions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(chckbxAllOperations.isChecked()) {
+                    sharedPrefs.edit().putString("operators", "all").apply();
+                } else {
+                    String operators = "";
+                    if (chckbxAddition.isChecked()) {
+                        operators += "+";
+                    }
+
+                    if (chckbxSubraction.isChecked()) {
+                        operators += "-";
+                    }
+
+                    if(chckbxMultiplication.isChecked()) {
+                        operators += "*";
+                    }
+
+                    if(chckbxDivision.isChecked()) {
+                        operators += "/";
+                    }
+
+                    sharedPrefs.edit().putString("operators", operators).apply();
+                }
+
+                sharedPrefs.edit().putInt("difficulty", spnnrDifficulty.getSelectedItemPosition())
+                        .apply();
+
+                if (rdoSingleOperation.isChecked()) {
+                    sharedPrefs.edit().putInt("operations", 0).apply();
+                } else {
+                    sharedPrefs.edit().putInt("operations", 1).apply();
+                }
+
+                Intent mainIntent = new Intent(OptionsActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+            }
+        });
+
+        // Leave the Options Menu
+        Button btnCancelOptions = (Button) findViewById(R.id.btnCancelOptions);
+        btnCancelOptions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(OptionsActivity.this, MainActivity.class);
+                startActivity(mainIntent);
             }
         });
     }
+
+    private static void setCheckboxes(boolean isEnabled, CheckBox[] checkboxes) {
+        if(isEnabled) {
+            for (CheckBox checkbox : checkboxes) {
+                checkbox.setClickable(true);
+                checkbox.setTextColor(Color.parseColor("#000000"));
+            }
+        } else {
+            for (CheckBox checkbox : checkboxes) {
+                checkbox.setChecked(false);
+                checkbox.setClickable(false);
+                checkbox.setTextColor(Color.parseColor("#CCCCCC"));
+            }
+        }
+    } // setCheckboxes(...)
+
 }
