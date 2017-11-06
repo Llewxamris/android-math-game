b50c1c18b12beac08cc8fafadd2d221234060c64
Maxwell Haley <maxwell.haley@openmailbox.org>
2017-11-06 13:24:23 -0500

    Add basic score and lives functionality
    Score now increases on a correct answer, and lives decreased on an
    incorrect answer. The timer is currently not implemented, and there is
    no lose state in the game yet.

--- app/src/main/java/ca/qc/cegep_heritage/mhaley_b51_mathexplosion/GameActivity.java
+++ app/src/main/java/ca/qc/cegep_heritage/mhaley_b51_mathexplosion/GameActivity.java
@@ -9,38 +9,117 @@ import android.widget.TextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
+//import java.util.Timer;
+//import java.util.TimerTask;
 
 public class GameActivity extends AppCompatActivity {
 
+    private Equation equation;
+    private int lives = 3;
+    private long time;
+    private int score = 0;
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        final SharedPreferences sharedPrefs = GameActivity.this.getSharedPreferences("options", Context.MODE_PRIVATE);
 
-        TextView txtEquation = (TextView) findViewById(R.id.txtEquation);
+        showNewEquation(sharedPrefs.getString("operators","+"),
+                sharedPrefs.getInt("operations", 1));
+        setTimer(sharedPrefs.getInt("difficulty", 2));
 
-        SharedPreferences sharedPrefs = GameActivity.this.getSharedPreferences("options", Context.MODE_PRIVATE);
-
-        final Equation eq = EquationGenerator.generateEquation(sharedPrefs
-                .getString("operators","+"),
-                    sharedPrefs.getInt("operations", 1));
-
-        txtEquation.setText(eq.getEquation());
+//        TimerTask playerTookToLong = getTimerTask(sharedPrefs);
+//
+//        Timer countDownTimer = new Timer("countDownTimer");
+//        countDownTimer.schedule(playerTookToLong, time);
 
         Button btnAnswer = (Button) findViewById(R.id.btnAnswer);
         btnAnswer.setOnClickListener(new View.OnClickListener() {
-            EditText edtxtUserAnswer = (EditText) findViewById(R.id.edtxtUserAnswer);
             @Override
             public void onClick(View v) {
-                if (eq.checkIsAnswer(Integer.parseInt(edtxtUserAnswer.getText().toString()))) {
+                EditText edtxtUserAnswer = (EditText) findViewById(R.id.edtxtUserAnswer);
+                if (equation.checkIsAnswer(Integer.parseInt(edtxtUserAnswer.getText().toString()))) {
                     Toast toast = Toast
                             .makeText(GameActivity.this, "Correct!", Toast.LENGTH_LONG);
                     toast.show();
+                    increaseScore();
+                } else {
+                    loseLife();
                 }
+                showNewEquation(sharedPrefs.getString("operators","+"),
+                        sharedPrefs.getInt("operations", 1));
             }
         });
+    }
 
+    private void showNewEquation(String operators, int operations) {
+        equation = EquationGenerator.generateEquation(operators, operations);
+
+        TextView txtEquation = (TextView) findViewById(R.id.txtEquation);
+        txtEquation.setText(equation.getEquation());
+    }
+
+    private void increaseScore() {
+        score++;
+
+        TextView txtScore = (TextView) findViewById(R.id.txtScore);
+        txtScore.setText(String.valueOf(score));
+
+        EditText edtxtUserAnswer = (EditText) findViewById(R.id.edtxtUserAnswer);
+        edtxtUserAnswer.setText("");
+
+        if(score % 5 == 0) {
+            time -= 5;
+        }
+    }
+
+    private void loseLife() {
+        lives--;
+        TextView txtLives = (TextView) findViewById(R.id.txtLives);
+        txtLives.setText(String.valueOf(lives));
+
+        EditText edtxtUserAnswer = (EditText) findViewById(R.id.edtxtUserAnswer);
+        edtxtUserAnswer.setText("");
+
+        if(lives == 0) {
+            // Lose condition
+        }
+    }
+
+//    private TimerTask getTimerTask(SharedPreferences sharedPrefs) {
+//        return new TimerTask() {
+//            @Override
+//            public void run() {
+//                loseLife();
+//                showNewEquation(sharedPrefs.getString("operators","+"),
+//                        sharedPrefs.getInt("operations", 1));
+//                Timer countDownTimer = new Timer("countDownTimer");
+//                countDownTimer.schedule(getTimerTask(), time);
+//            }
+//        };
+//    }
+
+    private void setTimer(int difficulty) {
+        switch (difficulty) {
+            case 0:
+                time = 60000;
+                break;
+            case 1:
+                time = 45000;
+                break;
+            case 2:
+                time = 30000;
+                break;
+            case 3:
+                time = 15000;
+                break;
+            case 4:
+                time = 5000;
+                break;
+            default:
+                time = 30000;
+                break;
+        }
     }
 }
